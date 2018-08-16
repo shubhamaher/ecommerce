05d928de92ed8bdd1929e2908a57e3645f58f31f
shubhamaher <er.shubhamaher@gmail.com>
2018-08-16 18:45:33 +0530

    Initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\shubham\dev\ecommerce\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ src/products/admin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display= ['__str__','slug']
+	class Meta:
+		model = Product
+
+admin.site.register(Product, ProductAdmin)
